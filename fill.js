3e3c292377f8af918924c2c41fbb3f0e0891c0e7
Peter roe <peterroe@163.com>
2021-11-24 12:26:18 +0800

    better fulfill

--- fill.js
+++ fill.js
@@ -1,15 +1,9 @@
 Array.prototype.myFill = function(value, start = 0, end = this.length) {
     if (start < 0) {
-        start = this.length + start
+        start = Math.max(this.length + start, 0)
     }
     if (end < 0) {
-        end = this.length + end
-    }
-    if (start < 0) {
-        start = 0
-    }
-    if (end > this.length) {
-        end = this.length
+        end = Math.min(this.length + end, this.length)
     }
     for (let i = start; i < end; i++) {
         this[i] = value
@@ -19,4 +13,4 @@ Array.prototype.myFill = function(value, start = 0, end = this.length) {
 
 //One example:
 let arr = [1, 2, 3, 4, 5]
-arr.myFill(0, 2, 4) //=>  [1, 2, 0, 0, 5]
+console.log(arr.myFill(0, 2, 4)) //=>  [1, 2, 0, 0, 5]
--- includes.js
+++ includes.js
@@ -3,10 +3,7 @@
  */
 Array.prototype.myIncludes = function(value, fromIndex = 0) {
     if (fromIndex < 0) {
-        fromIndex = this.length + fromIndex
-    }
-    if (fromIndex < 0) {
-        fromIndex = 0
+        fromIndex = Math.max(this.length + fromIndex, 0)
     }
     for (let i = fromIndex; i < this.length; i++) {
         if (this[i] === value || Number.isNaN(this[i]) && Number.isNaN(value)) {
